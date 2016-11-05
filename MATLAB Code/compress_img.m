ce7377fdec5020350709b2518bad965d4e9315aa
mayankroy <mayank.ry@gmail.com>
2016-11-05 15:27:27 -0400

    Linear Algebra MSF

---
+++ MATLAB Code/compress_img.m
@@ -0,0 +1,29 @@
+function X=compress_img(z)
+%Converting image into ntsc mode dividing the image into luminosity and
+%chrominance planes
+    luv=rgb2ntsc(z);
+    
+%Extracting the lumnious plane
+    L=luv(:,:,1);
+    
+%Taking single value decomposition of the plane
+    [U D V]=svd(L);
+    
+    A=zeros(size(L));
+    [M N]=size(L)
+    temp=M/10;
+%Using temp fourier coeffecients to reconstruct the image
+    for j=1:temp
+        
+%Applying the Moore-Penrose inverse to reconstruct
+    A=A+D(j,j)*U(:,j)*V(:,j)';
+    end
+
+%Storing the new luminous plane in the compressed ntsc planes
+    luv2(:,:,1)=A;
+    luv2(:,:,2)=luv(:,:,2);
+    luv2(:,:,3)=luv(:,:,3);
+    
+%Converting back to rgb
+    X=ntsc2rgb(luv2);
+end
